33ab1660ecce5d0d276988d2020d9682db3acb8f
leejunk828 <95489942+leejunk828@users.noreply.github.com>
2021-12-04 12:27:58 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,20 +4,19 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 
 seq = Sequential()
-seq.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+seq.add(LSTM(units = 16, return_sequences = True, input_shape = (X_train.shape[1], 1)))
 seq.add(Dropout(0.2))
-
-seq.add(LSTM(units = 50, return_sequences = True))
+seq.add(LSTM(units = 32, return_sequences = True))
 seq.add(Dropout(0.2))
-
-seq.add(LSTM(units = 50, return_sequences = True))
+seq.add(LSTM(units = 64, return_sequences = True))
 seq.add(Dropout(0.2))
-
-seq.add(LSTM(units = 50))
+seq.add(LSTM(units = 64, return_sequences = True))
 seq.add(Dropout(0.2))
-
+seq.add(LSTM(units = 32, return_sequences = True))
+seq.add(Dropout(0.2))
+seq.add(LSTM(units = 16))
+seq.add(Dropout(0.2))
 seq.add(Dense(units = 1))
 
 seq.compile(optimizer = 'adam', loss = 'mean_squared_error')
-
 seq.fit(X_train, y_train, epochs = 100, batch_size = 32)
